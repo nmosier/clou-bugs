cc2eb94095bb3a4401c35cd14f6e702e537178aa
Nicholas Mosier <nmosier@stanford.edu>
2022-02-14 14:32:56 -0800

    update

--- openssl/HASH_FINAL/bug.c
+++ openssl/HASH_FINAL/bug.c
@@ -1,8 +1,8 @@
 int HASH_FINAL(unsigned char *md, HASH_CTX *c)
 {
     ...
-    size_t n = c->num;
+    size_t n = c->num; // <<< speculative store bypass
 
-    p[n] = 0x80;                /* there is always room for one */
+    p[n] = 0x80; // <<< secret in `n` leaked through array access
     ...
 }
--- openssl/HASH_FINAL/fix.c
+++ openssl/HASH_FINAL/fix.c
@@ -4,8 +4,8 @@ int HASH_FINAL(unsigned char *md, HASH_CTX *c)
 {
     ...
     atomic_thread_fence(memory_order_acquire);
-    size_t n = c->num;
+    size_t n = c->num; // <<< speculative store bypass
 
-    p[n] = 0x80;                /* there is always room for one */
+    p[n] = 0x80; // <<< secret in `n` leaked through array access
     ...
 }
