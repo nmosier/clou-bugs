e1792d8bcf6eb1ef019038aca5907c32204a32c4
Nicholas Mosier <nmosier@stanford.edu>
2022-02-14 12:56:45 -0800

    initial commit

---
+++ libsodium/SHA512_Pad/bug.c
@@ -0,0 +1,21 @@
+static void
+SHA256_Pad(crypto_hash_sha256_state *state, uint32_t tmp32[64 + 8])
+{
+    unsigned int r;
+    unsigned int i;
+
+    r = (unsigned int) ((state->count >> 3) & 0x3f); // <<< speculative store bypass
+    if (r < 56) {
+        for (i = 0; i < 56 - r; i++) {
+            state->buf[r + i] = PAD[i]; // <<< secret `r` used as index into state->buf[]
+        }
+    } else {
+        for (i = 0; i < 64 - r; i++) {
+            state->buf[r + i] = PAD[i]; // <<< secret `r` used as index into state->buf[]
+        }
+        SHA256_Transform(state->state, state->buf, &tmp32[0], &tmp32[64]);
+        memset(&state->buf[0], 0, 56);
+    }
+    STORE64_BE(&state->buf[56], state->count);
+    SHA256_Transform(state->state, state->buf, &tmp32[0], &tmp32[64]);
+}
