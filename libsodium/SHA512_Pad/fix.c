bb917918c090ed72eb85ed26574e2fc907a5306e
Nicholas Mosier <nmosier@stanford.edu>
2022-02-14 13:00:55 -0800

    update

--- libsodium/SHA512_Pad/fix.c
+++ libsodium/SHA512_Pad/fix.c
@@ -1,24 +1,24 @@
 #include <stdatomic.h>
 
 static void
-SHA256_Pad(crypto_hash_sha256_state *state, uint32_t tmp32[64 + 8])
+SHA512_Pad(crypto_hash_sha512_state *state, uint64_t tmp64[80 + 8])
 {
     unsigned int r;
     unsigned int i;
 
     atomic_thread_fence(memory_order_acquire);
-    r = (unsigned int) ((state->count >> 3) & 0x3f); // <<< speculative store bypass
-    if (r < 56) {
-        for (i = 0; i < 56 - r; i++) {
-            state->buf[r + i] = PAD[i]; // <<< secret `r` used as index into state->buf[]
+    r = (unsigned int) ((state->count[1] >> 3) & 0x7f);
+    if (r < 112) {
+        for (i = 0; i < 112 - r; i++) {
+            state->buf[r + i] = PAD[i];
         }
     } else {
-        for (i = 0; i < 64 - r; i++) {
-            state->buf[r + i] = PAD[i]; // <<< secret `r` used as index into state->buf[]
+        for (i = 0; i < 128 - r; i++) {
+            state->buf[r + i] = PAD[i];
         }
-        SHA256_Transform(state->state, state->buf, &tmp32[0], &tmp32[64]);
-        memset(&state->buf[0], 0, 56);
+        SHA512_Transform(state->state, state->buf, &tmp64[0], &tmp64[80]);
+        memset(&state->buf[0], 0, 112);
     }
-    STORE64_BE(&state->buf[56], state->count);
-    SHA256_Transform(state->state, state->buf, &tmp32[0], &tmp32[64]);
+    be64enc_vect(&state->buf[112], state->count, 16);
+    SHA512_Transform(state->state, state->buf, &tmp64[0], &tmp64[80]);
 }
