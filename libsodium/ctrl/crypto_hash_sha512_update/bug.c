e72810eda0d915bb9806278cdd888c6de82915d9
Nicholas Mosier <nmosier@stanford.edu>
2022-02-15 06:43:22 -0800

    Update bug.c

--- libsodium/ctrl/crypto_hash_sha512_update/bug.c
+++ libsodium/ctrl/crypto_hash_sha512_update/bug.c
@@ -10,9 +10,9 @@ crypto_hash_sha512_update(crypto_hash_sha512_state *state,
     if (inlen <= 0U) {
         return 0;
     }
-    r = (unsigned long long) ((state->count[1] >> 3) & 0x7f);
+    r = (unsigned long long) ((state->count[1] >> 3) & 0x7f); // <<< speculative store bypass
     /* ... */
-    if (inlen < 128 - r) {
+    if (inlen < 128 - r) { // <<< insecure branch on secret `r`
         for (i = 0; i < inlen; i++) {
             state->buf[r + i] = in[i];
         }
