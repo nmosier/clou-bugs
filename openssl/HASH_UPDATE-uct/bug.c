1b62e45cb98d5aa5ac7dba48060e85966edb2984
Nicholas Mosier <nmosier@stanford.edu>
2022-02-14 21:11:30 -0800

    update

--- openssl/HASH_UPDATE-uct/bug.c
+++ openssl/HASH_UPDATE-uct/bug.c
@@ -1,19 +1,10 @@
 int HASH_UPDATE(HASH_CTX *c, const void *data_, size_t len)
 {
-    ...
-    size_t n;
-    ...
-    n = c->num; // <<< speculative store bypass
-    if (n != 0) {
-        p = (unsigned char *)c->data;
-
-        if (len >= HASH_CBLOCK || len + n >= HASH_CBLOCK) {
-            memcpy(p + n, data, HASH_CBLOCK - n); // <<< memcpy() dereferences secret-tainted pointer
-            ...
-        } else {
-            memcpy(p + n, data, len); // <<< memcpy() dereferences secret-tainted pointer
-            ...
-        }
-    }
-    ...
+    /* ... */
+    HASH_LONG l;
+    /* ... */
+    l = (c->Nl + (((HASH_LONG) len) << 3)) & 0xffffffffUL; // <<< speculative store bypass
+    if (l < c->Nl) // <<< branch on secret
+        c->Nh++;
+    /* ... */
 }
--- openssl/HASH_UPDATE-uct/fix.c
+++ openssl/HASH_UPDATE-uct/fix.c
@@ -2,21 +2,12 @@
 
 int HASH_UPDATE(HASH_CTX *c, const void *data_, size_t len)
 {
-    ...
-    size_t n;
-    ...
+    /* ... */
+    HASH_LONG l;
+    /* ... */
     atomic_thread_fence(memory_order_acquire);
-    n = c->num; // <<< speculative store bypass
-    if (n != 0) {
-        p = (unsigned char *)c->data;
-
-        if (len >= HASH_CBLOCK || len + n >= HASH_CBLOCK) {
-            memcpy(p + n, data, HASH_CBLOCK - n); // <<< memcpy() dereferences secret-tainted pointer
-            ...
-        } else {
-            memcpy(p + n, data, len); // <<< memcpy() dereferences secret-tainted pointer
-            ...
-        }
-    }
-    ...
+    l = (c->Nl + (((HASH_LONG) len) << 3)) & 0xffffffffUL; // <<< speculative store bypass
+    if (l < c->Nl) // <<< branch on secret
+        c->Nh++;
+    /* ... */
 }
